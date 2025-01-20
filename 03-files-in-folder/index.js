4c38bf277d6c5ae72dadd7a8baa80eea72a555b8
konstantinuza <kostya2.7phoenix@gmail.com>
2025-01-20 21:10:47 +0300

    feat: 03-files-in-filder completed

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir('secret-folder', (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    const filePath = path.join('secret-folder', file);
+    const fileName = path.parse(filePath).name;
+    const fileExt = path.parse(filePath).ext.slice(1);
+
+    fs.stat(filePath, (err, stats) => {
+      if (err) throw err;
+      console.log(`${fileName} - ${fileExt} - ${(stats.size / 1024).toFixed(3)}kb`);
+    });
+  });
+});
+
