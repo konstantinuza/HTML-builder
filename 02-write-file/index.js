328af6d3ffac1589c5de4497763a97e94c73821d
konstantinuza <kostya2.7phoenix@gmail.com>
2025-01-21 03:19:01 +0300

    fix: 02-write-file add ctrl+c reaction

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,7 +5,7 @@ fs.writeFile('text.txt', '', (err) => {
   if (err) throw err;
 });
 
-console.log(`--- Yo, human! Enter text to write to file!\n--- When you're done, enter "exit" to end recording -->>`);
+console.log(`--- Yo, human! Enter text to write to file!\n--- When you're done, enter "exit" or press (ctrl + c) to end recording -->>`);
 
 stdin.on('data', (data) => {
   const text = data.toString().trim();
@@ -21,8 +21,9 @@ process.on('exit', function () {
   console.log(`--- File writing is complete, it's easy for me.`);
 })
 
-
-
+process.on('SIGINT', () => {
+  exit();
+});
 
 
 
